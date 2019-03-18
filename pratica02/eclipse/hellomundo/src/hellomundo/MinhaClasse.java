ce19c58be5e143fa7b802a9839c31a7f61b5327e
Lucas Brancatelli <lucas.brancatelli@gmail.com>
2019-03-18 11:42:23 -0300

    testes IDE

---
+++ pratica02/eclipse/hellomundo/src/hellomundo/MinhaClasse.java
@@ -0,0 +1,14 @@
+package hellomundo;
+
+public class MinhaClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int idade = 15;
+		String nome =  "Emerson";
+		System.out.println("Nome: " + nome);
+		System.out.println("Idade: " + idade);
+		
+	}
+
+}
