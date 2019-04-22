29a0a58aad1ddc41fd127e5c48f5c5bb2caadaa3
LUCAS  BRANCATELLI SANTANA <17.00895-6@maua.br>
2019-04-22 12:11:29 -0300

    aula 2204

---
+++ pratica06/src/com/company/Gerente.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Gerente extends Funcionario {
+
+    private int senha;
+
+    public Gerente (String nome, int senha){
+        super(nome);
+        this.senha = senha;
+    }
+
+    public boolean autenticar( int senha){
+        return senha == this.senha;
+    }
+
+    @Override
+    public double comissao(){
+        return 0.6*super.salario;
+    }
+}
